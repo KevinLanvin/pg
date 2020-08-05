bfe54417faefb625f82c012283d963305902906e
Kevin Lanvin <info.lanvink@gmail.com>
2020-08-05 20:22:11 +0200

    Customize buttons and add text shadow to banner h1

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './Button.scss';
 
-const Button = ({name, link}) => (
-  <a href={link} className="button">{name}</a>
+const Button = ({name, link, alt}) => {
+  const classes = classNames({ 'button': true, 'button--alt': alt });
+    return (
+  <a
+    href={link}
+    className={classes}
+  >{name}</a>
 );
+};
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
+  alt: PropTypes.bool,
 };
 
 export default Button;
--- src/components/QuandConsulter/index.js
+++ src/components/QuandConsulter/index.js
@@ -10,7 +10,7 @@ const QuandConsulter = () => (
     <div className="quand-consulter__text">
       <h2>Dans quel cas me consulter ?</h2>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-      <Button name="Voir la liste des pathologies" link="#"></Button>
+      <Button name="Voir la liste des pathologies" link="#" alt></Button>
     </div>
   </section>
 );
