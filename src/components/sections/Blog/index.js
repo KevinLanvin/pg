696a107adf1325d6e6235bf5903caf3e3ce19b67
Kevin Lanvin <info.lanvink@gmail.com>
2020-08-12 19:53:18 +0200

    Add long-text support

--- src/components/sections/Blog/index.js
+++ src/components/sections/Blog/index.js
@@ -4,7 +4,7 @@ import Card from '../../Card';
 import './Blog.scss';
 
 const Blog = () => {
-  const description = 'Eo adducta re per lsauriam, rege Persarum bellis finitimis inligato repellenteque.';
+  const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   return (
     <section className="blog">
       <h2 className="center">Retrouvez les derniers articles</h2>
