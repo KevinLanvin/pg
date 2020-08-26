0bd9536161bbfe0161e594bc4630705ea2cda77d
Kevin Lanvin <info.lanvink@gmail.com>
2020-08-26 13:19:41 +0200

    Add navlink for soins-naturels in header

--- src/components/Header/NavBar/index.js
+++ src/components/Header/NavBar/index.js
@@ -14,7 +14,7 @@ const NavBar = () => (
         Services
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><switch><g><path d="M24 24H0V0h24v24z" fill="none"/><path className="chevron" d="M15.9 9.3L12 13.2 8.1 9.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l4.6 4.6c.4.4 1 .4 1.4 0l4.6-4.6c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0z"/></g></switch></svg></a>
       <span className="dropdown">
-        <span><a href="#">Soins naturels</a></span>
+        <span><NavLink to="/soins-naturels">Soins naturels</NavLink></span>
         <span><a href="#">Soins énergétiques</a></span>
         <span><a href="#">Kinésiologie</a></span>
         <span><a href="#">Arrêt du Tabac</a></span>
