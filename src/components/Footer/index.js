5b2d25bd887c0b29f91be72306d6a8034d6a9924
Kevin Lanvin <info.lanvink@gmail.com>
2020-09-09 00:17:15 +0200

    Add mobile footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import './Footer.scss';
 
-const Footer = () => (
+const Footer = ({ mobile = false }) => (
   <footer className="footer">
-    <div className="footer__top-bar">
-      <Link to="/mentions-legales" className="footer__link">Mentions Légales</Link>
-      <Link to="/contact" className="footer__link">Contact</Link>
-    </div>
+    { !mobile && (
+      <div className="footer__top-bar">
+        <Link to="/mentions-legales" className="footer__link">Mentions Légales</Link>
+        <Link to="/contact" className="footer__link">Contact</Link>
+      </div>
+    )}
     <div className="footer__bottom-bar">
+      { mobile && (
+        <>
+          <Link to="/contact" className="footer__link">Contact</Link>
+          <Link to="/mentions-legales" className="footer__link">Mentions Légales</Link>
+        </>
+      )}
       <p>Site réalisé par MGDG et Kévin Lanvin</p>
     </div>
   </footer>
 );
+
+Footer.propTypes = {
+  mobile: PropTypes.bool,
+};
 
 export default Footer;
