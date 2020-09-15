537a88933d716b46bfb556ff52ca588f2300f397
Kevin Lanvin <info.lanvink@gmail.com>
2020-09-15 17:25:10 +0200

    Change services div color when sublink is active

--- src/components/Header/NavBar/index.js
+++ src/components/Header/NavBar/index.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 
 import './NavBar.scss';
 import Logo from '../../../img/logo.svg';
 
 const NavBar = ({ mobile }) => {
+  let location = useLocation();
   const [menuVisible, setMenuVisible] = useState(false);
   const navMobileClass = classNames('header__mobile-menu', { 'header__mobile-menu--visible': menuVisible });
+  const servicesPaths = ['/soins-naturels', '/soins-energetiques', '/kinesiologie', '/arret-tabac' ];
+  const servicesClass = classNames({'active': servicesPaths.includes(location.pathname)});
+  const chevronClass = classNames('chevron', {'chevron--active': servicesPaths.includes(location.pathname)});
   const closeMenu = () => setMenuVisible(false)
 
   return (
@@ -19,9 +23,9 @@ const NavBar = ({ mobile }) => {
       {!mobile && (
         <>
           <span>
-            <div>
+            <div className={servicesClass}>
               Services
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><switch><g><path d="M24 24H0V0h24v24z" fill="none"/><path className="chevron" d="M15.9 9.3L12 13.2 8.1 9.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l4.6 4.6c.4.4 1 .4 1.4 0l4.6-4.6c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0z"/></g></switch></svg></div>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><switch><g><path d="M24 24H0V0h24v24z" fill="none"/><path className={chevronClass} d="M15.9 9.3L12 13.2 8.1 9.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l4.6 4.6c.4.4 1 .4 1.4 0l4.6-4.6c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0z"/></g></switch></svg></div>
             <span className="dropdown">
               <span><NavLink to="/soins-naturels">Soins naturels</NavLink></span>
               <span><NavLink to="/soins-energetiques">Soins énergétiques</NavLink></span>
