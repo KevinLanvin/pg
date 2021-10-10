09f93e47f4aa2d3c2112e2ae4e7969b6233c1f39
Kevin Lanvin <klanvin@spreadthelive.com>
2021-10-10 19:21:33 +0200

    Remove shitty map

--- src/components/MyMap/index.js
+++ src/components/MyMap/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Map, Marker, TileLayer } from 'react-leaflet';
+import React from 'react'
+import PropTypes from 'prop-types'
+// import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import './myMap.scss'
 
-import './myMap.scss';
-
-const MyMap = ({ latitude, longitude, mobile = false }) => {
-  const position = [ latitude, longitude ];
-  return (
-    <Map center={position} zoom={14} style={{ width: '100%', height: '100%' }} dragging={!mobile}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      <Marker className="leaflet-container__marker" position={position} />
-    </Map>
-  );
-};
+const MyMap = ({ latitude, longitude, mobile = false }) => (
+	<></>
+	// <MapContainer center={[latitude, longitude]} zoom={14}>
+	// 	<TileLayer
+	// 		attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	// 		url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+	// 	/>
+	// 	<Marker
+	// 		className='leaflet-container__marker'
+	// 		position={{ lat: latitude, lon: longitude }}
+	// 	/>
+	// </MapContainer>
+)
 
 MyMap.propTypes = {
-  latitude: PropTypes.number.isRequired,
-  longitude: PropTypes.number.isRequired,
-  mobile: PropTypes.bool,
-};
+	latitude: PropTypes.number.isRequired,
+	longitude: PropTypes.number.isRequired,
+	mobile: PropTypes.bool,
+}
 
-export default MyMap;
+export default MyMap
--- src/components/sections/ContactSection/index.js
+++ src/components/sections/ContactSection/index.js
@@ -1,61 +1,73 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import LinkedInIcon from '../../../icons/linkedin-violet.svg';
-import FacebookIcon from '../../../icons/facebook-violet.svg';
-import InstagramIcon from '../../../icons/instagram-violet.svg';
-import Map from '../../MyMap';
+import React from 'react'
+import PropTypes from 'prop-types'
+import LinkedInIcon from '../../../icons/linkedin-violet.svg'
+import FacebookIcon from '../../../icons/facebook-violet.svg'
+import InstagramIcon from '../../../icons/instagram-violet.svg'
+import Map from '../../MyMap'
 
-import './contactSection.scss';
+import './contactSection.scss'
 
 const ContactSection = ({
-  title,
-  phone,
-  email,
-  address1,
-  address2,
-  facebookLinkTo,
-  linkedInLinkTo,
-  instagramLinkTo,
-  latitude,
-  longitude,
-  mobile = false,
+	title,
+	phone,
+	email,
+	address1,
+	address2,
+	facebookLinkTo,
+	linkedInLinkTo,
+	instagramLinkTo,
+	latitude,
+	longitude,
+	mobile = false,
 }) => (
-  <section className="contact-section">
-    <div className="contact-section__card-wrapper">
-      <div className="contact-section__card">
-        <h2>{title}</h2>
-        <p>{phone}</p>
-        <p><a href={`mailto:${email}`}>{email}</a></p>
-        <p>{address1}<br/>{address2}</p>
-        <p>
-          <a href={instagramLinkTo} target="_blank" rel="noopener noreferrer"><img src={InstagramIcon} alt="Instagram Pauline Gorlier" /></a>
-          <a href={linkedInLinkTo} target="_blank" rel="noopener noreferrer"><img src={LinkedInIcon} alt="Linked In Pauline Gorlier" /></a>
-          <a href={facebookLinkTo} target="_blank" rel="noopener noreferrer"><img src={FacebookIcon} alt="Facebook Pauline Gorlier" /></a>
-        </p>
-      </div>
-    </div>
-    <div className="contact-section__map">
-      <Map
-        latitude={Number.parseFloat(latitude)}
-        longitude={Number.parseFloat(longitude)}
-        mobile={mobile}
-      />
-    </div>
-  </section>
-);
+	<section className='contact-section'>
+		<div className='contact-section__card-wrapper'>
+			<div className='contact-section__card'>
+				<h2>{title}</h2>
+				<p>{phone}</p>
+				<p>
+					<a href={`mailto:${email}`}>{email}</a>
+				</p>
+				<p>
+					{address1}
+					<br />
+					{address2}
+				</p>
+				<p>
+					<a href={instagramLinkTo} target='_blank' rel='noopener noreferrer'>
+						<img src={InstagramIcon} alt='Instagram Pauline Gorlier' />
+					</a>
+					<a href={linkedInLinkTo} target='_blank' rel='noopener noreferrer'>
+						<img src={LinkedInIcon} alt='Linked In Pauline Gorlier' />
+					</a>
+					<a href={facebookLinkTo} target='_blank' rel='noopener noreferrer'>
+						<img src={FacebookIcon} alt='Facebook Pauline Gorlier' />
+					</a>
+				</p>
+			</div>
+		</div>
+		{/* <div className='contact-section__map'>
+			<Map
+				latitude={Number.parseFloat(latitude)}
+				longitude={Number.parseFloat(longitude)}
+				mobile={mobile}
+			/>
+		</div> */}
+	</section>
+)
 
 ContactSection.propTypes = {
-  title: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  address1: PropTypes.string.isRequired,
-  address2: PropTypes.string.isRequired,
-  facebookLinkTo: PropTypes.string.isRequired,
-  linkedInLinkTo: PropTypes.string.isRequired,
-  instagramLinkTo: PropTypes.string.isRequired,
-  latitude: PropTypes.string.isRequired,
-  longitude: PropTypes.string.isRequired,
-  mobile: PropTypes.bool,
-};
+	title: PropTypes.string.isRequired,
+	phone: PropTypes.string.isRequired,
+	email: PropTypes.string.isRequired,
+	address1: PropTypes.string.isRequired,
+	address2: PropTypes.string.isRequired,
+	facebookLinkTo: PropTypes.string.isRequired,
+	linkedInLinkTo: PropTypes.string.isRequired,
+	instagramLinkTo: PropTypes.string.isRequired,
+	latitude: PropTypes.string.isRequired,
+	longitude: PropTypes.string.isRequired,
+	mobile: PropTypes.bool,
+}
 
-export default ContactSection;
+export default ContactSection
